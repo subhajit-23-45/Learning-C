abf10e696f356f7629ff956eada641a87cad0eb7
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 19:50:42 +0530

    Multiple Outputs

---
+++ Codechef/course/LBCL02_problems_CLB007.c
@@ -0,0 +1,15 @@
+//Write a program which does the following
+//
+//Add a printf statement and output the sum of 3 + 4
+//Add another printf statement and output the sum of 2 + 1.
+//Use the printf syntax we learned in last problem.
+
+#include <stdio.h>
+
+int main() {
+	// add two printf statements here
+	printf("%d",3+4);
+	printf("%d",2+1);
+	return 0;
+	
+}
