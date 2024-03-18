6af2ed34f670032e98563beaee379565d77df002
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 20:54:01 +0530

    MCQ6

---
+++ Codechef/course/LBCL04_problems_CLB020.c
@@ -0,0 +1,23 @@
+// What is the difference between
+
+// #include <stdio.h>
+
+// int main() {
+ 
+//   printf("%d\n",12);
+//   printf("%d\n",11);
+
+// }
+// and
+
+// #include <stdio.h>
+
+// int main() {
+ 
+//   printf("%d\n",12);
+
+//   printf("%d\n",11);
+
+// }
+
+Answer is The two codes are equivalent
