46cd9cc5c108eb779119d0ab3f919897b7ee34b7
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 20:46:38 +0530

    Division

---
+++ Codechef/course/LBCL04_problems_CLB018.c
@@ -0,0 +1,17 @@
+// Write a program which does the following
+
+// Print the result of subtraction of 9 and 2
+// Print the result of division of 30 by 5
+// Output both results on separate lines
+
+#include <stdio.h>
+
+int main() {
+	// Print the result of 9 - 2
+	
+	printf("%d\n",9-2);
+		// Print the result of 30 / 5
+	printf("%d\n", 30/5);
+	return 0;
+	
+}
