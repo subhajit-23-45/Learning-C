9d10e86ba5b61d840a118e51ac7f24b079db08a0
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 21:17:16 +0530

    MCQ

---
+++ Codechef/practice/PCL01_problems_CPRACMCQ1.c
@@ -0,0 +1,8 @@
+// In C, what statement do you use to output the number 42 to the screen?
+
+Select one of the following options.
+printf(42);
+printf("%d", 42);
+print(42);
+
+The anser is printf("%d", 42);
