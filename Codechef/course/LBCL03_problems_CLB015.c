c2a1dc0065311172fd9c465a145f57176084419d
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 20:26:43 +0530

    MCQ4

---
+++ Codechef/course/LBCL03_problems_CLB015.c
@@ -0,0 +1,14 @@
+// What will be the output of this code?
+
+// #include <stdio.h>
+
+// int main() {
+//   printf("%d%d %d", 82, 2, 3);
+// }
+
+Select one of the following options.
+82 2  3
+8223
+822 3
+822" "3
+The answer is 822 3
