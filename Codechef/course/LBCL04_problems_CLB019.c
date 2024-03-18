1264124156331da3c1158093788281f2e709906a
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 20:51:04 +0530

    Syntax

---
+++ Codechef/course/LBCL04_problems_CLB019.c
@@ -0,0 +1,9 @@
+// Write the exact code which we discussed in this lesson, to output the sum of 12 and 7. If you miss anything like curly braces, you will get an error, don't worry, just ask about the error in Help tab.
+
+// Write complete code to print the sum of 12 and 7
+#include <stdio.h>
+
+int main() {
+    printf("%d\n",12+7);
+    return 0;
+}
