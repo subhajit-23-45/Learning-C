ed39a95e12449e17c4bf43202e45e994f1e0b01e
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 20:59:28 +0530

    Declaring a Variable

---
+++ Codechef/course/LBCL05_problems_CLB023.c
@@ -0,0 +1,17 @@
+// Write a program which does the following
+
+// Create an integer variable named number
+// Assign the value 19 to number
+// Output the value of number.
+
+#include <stdio.h>
+
+int main() {
+    // Declare a variable
+    int number = 19;
+    
+    // Print the variable
+    printf("%d", number);
+    return 0;
+  
+}
