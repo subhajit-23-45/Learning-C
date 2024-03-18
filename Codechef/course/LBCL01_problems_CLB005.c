feabbd9d6d385ccc4dceaaf20344138ac75f2d07
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 19:41:39 +0530

    MCQ

---
+++ Codechef/course/LBCL01_problems_CLB005.c
@@ -0,0 +1,8 @@
+//Which line of code will output the string "Hi"?
+
+Which line of code will output the string "Hi"?
+print(Hi);
+printf(Hi);
+print("Hi");
+printf("Hi");
+the answer is: printf("Hi");
