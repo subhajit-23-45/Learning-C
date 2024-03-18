cb515a83897b6fe040565103f06672cc7ac4aced
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 23:52:08 +0530

    4th hackerrank

---
+++ hackerrank/challenges_pointer-in-c_problem.c
@@ -0,0 +1,23 @@
+// Complete the function void update(int *a,int *b). It receives two integer pointers, int* a and int* b. Set the value of  to their sum, and  to their absolute difference. There is no return value, and no return statement is needed.
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function    
+int sum,diff;
+     sum = *a+*b;
+     diff = abs(*a-*b);
+     *a = sum;
+     *b = diff;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
