e51323f38bb8b18d04425fe7bd7a5c58d9a9b363
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 20:23:19 +0530

    Inserting Space between outputs

---
+++ Codechef/course/LBCL03_problems_CLB013.c
@@ -0,0 +1,10 @@
+// Write a program to output 7 and 3 on a single line, space separated, using a single printf statement.
+
+#include <stdio.h>
+
+int main() {
+
+    // Add two %d to print two numbers
+    printf("%d %d", 7, 3);
+	return 0;
+}
