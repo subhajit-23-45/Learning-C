8b49f63f03e74329661347d59209e60d6f819bac
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 21:28:03 +0530

    MCQ3

---
+++ Codechef/practice/PCL03_problems_CPRACMCQ5.c
@@ -0,0 +1,25 @@
+// In C, what statement do you use to check whether a variable 
+// �
+// A is equal to 2?
+// And if it is, print 9.
+
+// Option 1:
+
+// if(a=2):
+//     printf("9");
+// Option 2:
+
+// if(a==2):
+//     printf("9");
+// Option 3:
+
+// if(a==2) {
+//     printf("9");
+// }
+// Option 4:
+
+// if(a=2) {
+//     printf("9");
+// }
+
+The answer is option 3
