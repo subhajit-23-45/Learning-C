1b449638d1961084876acbbf5cd281fdd4fd5a32
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 19:43:59 +0530

    MCQ2

---
+++ Codechef/course/LBCL01_problems_CLB006.c
@@ -0,0 +1,8 @@
+//Which line of code will output the sum of 7 and 19?
+
+Which line of code will output the sum of 7 and 19?
+Printf(7 + 19)
+printf("%d",7 + 19);
+printf("7 + 19");
+printf(%d,7 + 19);
+The answer is : printf("%d",7+19);
