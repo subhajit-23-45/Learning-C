898e4eccb192c9f69bd2e711c821d6849fd8625b
subhajit-23-45 <subhajitpaul0408@gmail.com>
2024-03-18 20:29:27 +0530

    MCQ5

---
+++ Codechef/course/LBCL03_problems_CLB016.c
@@ -0,0 +1,14 @@
+// What will be the output of this code?
+
+// #include <stdio.h>
+
+// int main() {
+//   printf("Add%d to%d", 2, 3);
+// }
+
+Select one of the following options.
+Add2to3
+Add 2 to 3
+Add2 to3
+Add2to 3
+The answe is: Add2 to3
